873b09c3b552e4ec68ecbcc6d349a5c52ab190a1
Jens <jenswilliversen@gmail.com>
2020-11-09 16:26:28 +0100

    bugfix in Field.java

--- src/main/java/Spil/Field.java
+++ src/main/java/Spil/Field.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Field {
-    private static String fieldName;
+    private String fieldName;
 
 
     public Field(String fileName, String language){
@@ -39,7 +39,7 @@ public class Field {
 
     }
 
-    public static String getFieldName() {
+    public String getFieldName() {
         return fieldName;
     }
 }
--- src/main/java/Spil/Launcher.java
+++ src/main/java/Spil/Launcher.java
@@ -7,15 +7,23 @@ public class Launcher {
     public static void main(String[] args) {
         String lang = "English";
         fileScanner fieldScanner = new fileScanner("Fields");
+
         String[] fieldFileNames = fieldScanner.getFieldNames();
         Field[] allFields = new Field[fieldFileNames.length];
 
 
-        for(int i = 0; i< fieldFileNames.length;i++){
-            allFields[i] = new Field(fieldFileNames[i], lang);
+        for(int i=0; i< fieldFileNames.length;i++){
+            Field test = new Field(fieldFileNames[i], lang);
+            System.out.println(test.getFieldName());
+            allFields[i] = test;
         }
 
-        System.out.println(allFields[0].getFieldName());
-        System.out.println(allFields[1].getFieldName());
+        System.out.println("test1 udprint: " + allFields[0].getFieldName());
+        System.out.println("test2 udprint: " + allFields[1].getFieldName());
+
+
+
+        System.out.println("navn på fil 1: " + fieldFileNames[0]);
+        System.out.println("navn på fil 2: "+fieldFileNames[1]);
     }
 }
