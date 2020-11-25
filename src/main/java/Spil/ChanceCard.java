322dd107c21c3a58ea62149cf68903fa5984b711
baldm <63373006+baldm@users.noreply.github.com>
2020-11-25 14:52:33 +0100

    Fixet chance kort

---
+++ src/main/java/Spil/ChanceCard.java
@@ -0,0 +1,51 @@
+package Spil;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ChanceCard {
+    private String cardName;
+    private int cardID;
+    private boolean forceBuy;
+    private boolean drawCard;
+
+
+    public ChanceCard(String fileName, Language language){
+        try {
+            FileInputStream input = new FileInputStream("./Fields/"+ fileName);
+
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            cardName = prop.getProperty("name");
+
+            //cardName = language.getName(cardName);
+
+            cardID = Integer.parseInt(prop.getProperty("cardID"));
+
+            forceBuy = Boolean.parseBoolean(prop.getProperty("forceBuy"));
+
+            drawCard = Boolean.parseBoolean(prop.getProperty("drawCard"));
+
+
+        } catch (FileNotFoundException e){
+
+        } catch (IOException e){
+
+        }
+
+
+
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public int getCardID() {
+        return cardID;
+    }
+}
